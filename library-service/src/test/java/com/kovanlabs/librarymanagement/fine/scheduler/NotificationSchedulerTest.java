7a44598ccfa4b03051fd1b9a09e82e94231e48b6
thaniyarasi-10 <thaniyarasik@gmail.com>
2026-08-28 11:10:41 +0530

    Renamed test class name from to generic NotificationSchedulerTest

--- library-service/src/test/java/com/kovanlabs/librarymanagement/fine/scheduler/NotificationSchedulerTest.java
+++ library-service/src/test/java/com/kovanlabs/librarymanagement/fine/scheduler/NotificationSchedulerTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-class OverdueNotificationSchedulerTest {
+class NotificationSchedulerTest {
 
     private BorrowRepository borrowRepository;
     private SqsNotificationProducer sqsNotificationProducer;
